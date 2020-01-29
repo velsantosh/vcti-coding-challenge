54a6d84367d7a2c90772d259de42ab3b8b5b7ab4
Santosh Verma <santover@cisco.com>
2020-01-29 11:08:59 +0530

    ersantoshverma@gmail.com
    added distributed logging support.

--- vcti-coding-tool-cct-service/src/main/java/com/vcti/ct/CCTServices/CctServiceApplication.java
+++ vcti-coding-tool-cct-service/src/main/java/com/vcti/ct/CCTServices/CctServiceApplication.java
@@ -2,6 +2,8 @@ package com.vcti.ct.CCTServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
@@ -9,6 +11,8 @@ import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableFeignClients("com.hackathon.cctService")
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class CctServiceApplication {
 
 	public static void main(String[] args) {
--- vcti-coding-tool-srv-service/src/main/java/com/hackathon/SRVServices/SrvServiceApplication.java
+++ vcti-coding-tool-srv-service/src/main/java/com/hackathon/SRVServices/SrvServiceApplication.java
@@ -2,6 +2,8 @@ package com.hackathon.SRVServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
@@ -9,6 +11,8 @@ import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableFeignClients("com.vcti.ct.SRVServices")
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class SrvServiceApplication {
 
 	public static void main(String[] args) {
