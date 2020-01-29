dfb9d5cd7a6f95081f02ce112f9c8380906aa3b8
Santosh Verma <santover@cisco.com>
2020-01-29 14:02:43 +0530

    ersantoshverma@gmail.com
    logback logging related changes

--- vcti-coding-tool-aa-service/src/main/java/com/vcti/ct/AAServices/controller/UserController.java
+++ vcti-coding-tool-aa-service/src/main/java/com/vcti/ct/AAServices/controller/UserController.java
@@ -27,56 +27,67 @@ public class UserController {
 
 	@GetMapping(value = "/healthcheck", produces = "application/json; charset=utf-8")
 	public String getHealthCheck() {
+		LOG.debug("Calling getHealthCheck method");
 		return "{ \"isWorking\" : true }";
 	}
 
 	@GetMapping("/users")
 	public List<User> getUsers() {
+		LOG.debug("Calling getUsers method");
 		return userDataService.getUsers();
 	}
 
 	@GetMapping("/user/{id}")
 	public Optional<User> getUser(@PathVariable String id) {
+		LOG.debug("Calling getUser method");
 		return userDataService.getUser(id);
 	}
 
 	@GetMapping("/user/uname/{uName}")
 	public User getUserByUserName(@PathVariable String uName) {
+		LOG.debug("Calling getUserByUserName method");
 		return userDataService.getUserByUserName(uName);
 	}
 	
 	@PutMapping("/user/{id}")
 	public Optional<User> updateUser(@RequestBody User newUser, @PathVariable String id) {
+		LOG.debug("Calling updateUser method");
 		return userDataService.updateUser(newUser, id);
 	}
 	
 	@PutMapping("/user/uname/{uName}")
 	public User updateUserUsingUserName(@RequestBody User newUser, @PathVariable String uName) {
+		LOG.debug("Calling updateUserUsingUserName method");
 		return userDataService.updateUserUsingUserName(newUser, uName);
 	}
 
 	@DeleteMapping(value = "/user/{id}", produces = "application/json; charset=utf-8")
 	public String deleteUser(@PathVariable String id) {
+		LOG.debug("Calling deleteUser method");
 		return userDataService.deleteUser(id);
 	}
 
 	@PostMapping("/user")
 	public User addUser(@RequestBody User newUser) {
+		LOG.debug("Calling addUser method");
 		return userDataService.addUser(newUser);
 	}
 
 	@GetMapping("/perm/{id}")
 	public List<String> getPermissionsByUser(@PathVariable String id) {
+		LOG.debug("Calling getPermissionsByUser method");
 		return userDataService.getPermissionsById(id);
 	}
 
 	@GetMapping("/permByUserName/{userName}")
 	public List<String> getPermissionsByUserName(@PathVariable String userName) {
+		LOG.debug("Calling getPermissionsByUserName method");
 		return userDataService.getPermissionsByUserName(userName);
 	}
 
 	@GetMapping("/validateLogin/{userName}/{password}")
 	public boolean validateLogin(@PathVariable String userName, @PathVariable String password) {
+		LOG.debug("Calling validateLogin method");
 		return userDataService.validateLogin(userName, password);
 	}
 
